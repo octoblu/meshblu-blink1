cf6ddfa512f680ce368926e1368db3154a68c011
Aaron Herres and Roy van de Water <sqrt+aaron+roy@octoblu.com>
2015-03-30 16:00:27 -0700

    Auto deploy on travis

--- pre-compile.js
+++ pre-compile.js
@@ -1,16 +1,18 @@
-var fs   = require('fs');
-var os   = require('os');
-var zlib = require('zlib');
-var tar  = require('tar');
+var fs      = require('fs-extra');
+var os      = require('os');
+var path    = require('path');
+var zlib    = require('zlib');
+var tar     = require('tar');
 var fstream = require('fstream');
 
 var getFilename = function(packageJSON){
   return [packageJSON.name, packageJSON.version, os.platform(), os.arch(), 'node-modules'].join('-') + '.tar.gz';
 };
 
+fs.mkdirpSync('build');
 var filename = getFilename(require('./package.json'));
-console.log(filename);
-fstream.Reader({ path: __dirname, type: "Directory" })
+
+fstream.Reader({path: __dirname, type: "Directory"})
        .pipe(tar.Pack())
        .pipe(zlib.Gzip())
-       .pipe(fs.createWriteStream(filename));
+       .pipe(fs.createWriteStream(path.join('build', filename)));
